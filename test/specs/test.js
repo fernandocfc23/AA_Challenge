9c7dde4b891de24803c5dedf5508440a006336ba
Fernando Gómez <fernandocfc22@outlook.com>
2020-07-18 12:27:18 -0600

    Fixes

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,9 +1,13 @@
-var webdriverio = require('webdriverio')
-var options = { desiredCapabilities: {browserName: 'Chrome'}}
-var cliente = webdriverio.remote(options);
+describe('challenge page', () => {
+    it('should have the right title', () => {
+        browser.url('http://www.shino.de/parkcalc/')
+        expect(browser).toHaveTitle('Parking Cost Calculator');
+    })
+    it('should have clickable elements', () => {
+        const startingDate = $('#StartingDate')
+		expect(startingDate).toBeClickable()
+        const leavingDate = $('#LeavingDate')
+		expect(leavingDate).toBeClickable()
+    })
 
-cliente
-	.init()
-	.url('http://www.shino.de/parkcalc/')
-	.submitForm('Calculator')
-	.end();
+})

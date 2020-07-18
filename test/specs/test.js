200b97b27a9ebd5a4c8c9f0eb8389c3c94d21e47
Fernando Gómez <fernandocfc22@outlook.com>
2020-07-18 15:49:30 -0600

    testing a user case

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,4 +1,4 @@
-describe('challenge page', () => {
+describe('challenge test', () => {
     it('should have the right title', () => {
         browser.url('http://www.shino.de/parkcalc/')
         expect(browser).toHaveTitle('Parking Cost Calculator');
@@ -9,5 +9,23 @@ describe('challenge page', () => {
         const leavingDate = $('#LeavingDate')
 		expect(leavingDate).toBeClickable()
     })
-
+    it('should form have clickable elements', () => {
+	    const links = $$('Calculator')
+	    links.forEach((link) => {
+	        link.click()
+	    })
+	})
+    it('should set value for a certain element', () => {
+	    const startingDate = $('#StartingDate');
+	    startingDate.setValue('18/07/2020');
+	    const startingTime = $('#StartingTime');
+	    startingTime.setValue(Math.floor((Math.random() * 6)+ 1)+":00");
+	    const startingDate = $('#LeavingDate');
+	    startingDate.setValue('18/07/2020');
+	    const startingTime = $('#LeavingTime');
+	    startingTime.setValue("8:00");
+	    const myInput = $('input')
+		expect(myInput).toHaveValue('us')
+    })
 })
+
